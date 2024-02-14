b4624d6bac13dbd391f62b83eb735c43b3c160d2
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-02-14 14:41:07 +0200

    Update keb.py

--- keb.py
+++ keb.py
@@ -1,9 +1,30 @@
-import telebot
-import random
+from telebot import *
+from telebot.types import *
+from time import sleep, time
+from database_main import *
+
+# from keepAlive import keep_alive
+
+
+import telebot, time
+from time import time
+from telebot import types
+from functions_to_my_bots import *
 import messagesBots
 
-@bot.message_handler(commands=["almortagel"])
-def startt(message):
+
+def send_member_private(message: types.Message):
+    global cv, vc
+    mtxt = message.text
+    CHid = message.from_user.id
+            if mtxt == "/almortagel":
+            bot.send_message(
+                message.chat.id,
+                text=messagesBots.message_for_admin,
+                parse_mode="HTML",
+                reply_to_message_id=message.id,
+                reply_markup=mycommands_on(),
+            )
     mrk = ReplyKeyboardMarkup(row_width=6)
 
     ttns = [
@@ -27,11 +48,10 @@ def startt(message):
 def msgs(message):
     text = message.text
     if text == "تلاو" or text == "تلاوات" or text == "تلاوة":
-        voicerurl = "https://t.me/EIEI06/" + str(random.randint(24, 618))
-        bot.send_voice(message.chat.id, voicerurl, caption="« صلي على سيدنا محمد ﷺ »", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
+        voice_url = "https://t.me/EIEI06/" + str(random.randint(24, 618))
+        bot.send_voice(message.chat.id, voice_url, caption="« صلي على سيدنا محمد ﷺ »", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
             telebot.types.InlineKeyboardButton('✧ - المطور 🌐', url='https://t.me/Almortagel_12'),
             telebot.types.InlineKeyboardButton('✧ - قناة مطور البوت', url='https://t.me/AlmortagelTech')
-
 @bot.message_handler(func=lambda message: True)
 def msgs(message):
     text = message.text
@@ -40,21 +60,20 @@ def msgs(message):
         bot.send_voice(message.chat.id, voice_url, caption="🥹♥ ¦ تـم اختيـار الشيخ عبدالباسط لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
             telebot.types.InlineKeyboardButton('✧ - المطور 🌐', url='https://t.me/Almortagel_12'),
             telebot.types.InlineKeyboardButton('✧ - قناة مطور البوت', url='https://t.me/AlmortagelTech')
-
 @bot.message_handler(func=lambda message: True)
 def msgs(message):
     text = message.text
     if text == "استوري" or text == "استوريات":
-        voicer_url = "https://t.me/yoipopl/" + str(random.randint(2, 140))
-        bot.send_voice(message.chat.id, voicer_url, caption="🥹♥ ¦ تـم اختيـار استوري لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
+        voice_url = "https://t.me/yoipopl/" + str(random.randint(2, 140))
+        bot.send_voice(message.chat.id, voice_url, caption="🥹♥ ¦ تـم اختيـار استوري لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
             telebot.types.InlineKeyboardButton('✧ - المطور 🌐', url='https://t.me/Almortagel_12'),
             telebot.types.InlineKeyboardButton('✧ - قناة مطور البوت', url='https://t.me/AlmortagelTech')
 @bot.message_handler(func=lambda message: True)
 def msgs(message):
     text = message.text
     if text == "نقشبندي" or text == "الشيخ نقشبندي":
-        voiceurl = "https://t.me/ggcnjj/" + str(random.randint(2, 114))
-        bot.send_voice(message.chat.id, voiceurl, caption="🥹♥ ¦ تـم اختيـار الشيخ نقشبندي لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
+        voice_url = "https://t.me/ggcnjj/" + str(random.randint(2, 114))
+        bot.send_voice(message.chat.id, voice_url, caption="🥹♥ ¦ تـم اختيـار الشيخ نقشبندي لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
             telebot.types.InlineKeyboardButton('✧ - المطور 🌐', url='https://t.me/Almortagel_12'),
             telebot.types.InlineKeyboardButton('✧ - قناة مطور البوت', url='https://t.me/AlmortagelTech')            
 
@@ -62,11 +81,10 @@ def msgs(message):
 def msgs(message):
     text = message.text
     if text == "افاتار شباب" or text == "صور شباب":
-        voiceee_url = "https://t.me/vgbmm/" + str(random.randint(2, 148))
-        bot.send_photo(message.chat.id, voiceee_url, caption="🥹♥ ¦ تـم اختيـار افاتار شباب لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
+        voicee_url = "https://t.me/vgbmm/" + str(random.randint(2, 148))
+        bot.send_photo(message.chat.id, voicee_url, caption="🥹♥ ¦ تـم اختيـار افاتار شباب لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
             telebot.types.InlineKeyboardButton('✧ - المطور 🌐', url='https://t.me/Almortagel_12'),
             telebot.types.InlineKeyboardButton('✧ - قناة مطور البوت', url='https://t.me/AlmortagelTech') 
-
 @bot.message_handler(func=lambda message: True)
 def msgs(message):
     text = message.text
@@ -75,3 +93,4 @@ def msgs(message):
         bot.send_photo(message.chat.id, voicee_url, caption="🥹♥ ¦ تـم اختيـار افاتار بنات لـك", reply_to_message_id=message.message_id, reply_markup=telebot.types.InlineKeyboardMarkup().row(
             telebot.types.InlineKeyboardButton('✧ - المطور 🌐', url='https://t.me/Almortagel_12'),
             telebot.types.InlineKeyboardButton('✧ - قناة مطور البوت', url='https://t.me/AlmortagelTech') 
+            
