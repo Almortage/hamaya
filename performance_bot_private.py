8a97ac64c165c426320d78b63a4849c205dfe164
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-02-14 11:02:27 +0200

    Update performance_bot_private.py

--- performance_bot_private.py
+++ performance_bot_private.py
@@ -60,16 +60,22 @@ def mycommands_on():
 
     btns = [
         KeyboardButton(text=f"اشعار الدخول {TONE} "),
-        KeyboardButton(text="قسم الاشتراك الاجباري")
+        KeyboardButton(text="قسم الاشتراك الاجباري"),
+    ]
+       btnsd = [
         KeyboardButton(text="الاحصائيات"),
-        KeyboardButton(text="رساله الترحيب")
+        KeyboardButton(text="رساله الترحيب"),
+    ]
+       btnss = [
         KeyboardButton(text="الاذاعة"),
-        KeyboardButton(text="الادمينة")
+        KeyboardButton(text="الادمينة"),
         KeyboardButton(text="المحظورين"),
         KeyboardButton(text="الردود العامة"),
         KeyboardButton(text="اخفاء"),
     ]
     mrk.add(*btns)
+    mrk.add(*btnsd)
+    mrk.add(*btnss)
     return mrk
 
 
@@ -208,7 +214,7 @@ def join_members(message: types.Message):
                 username = user.username
                 username = username if user.username else "غير معروف"
 
-                text = f"""تم انظمام مسخدم جديد الى البوت \n الاسم: {name} \n الايدي: {id} \n المعرف: {username}"""
+                text = f"""تم انظمام مسخدم جديد الى البوت \n الاسم: {name} \n الايدي: {id} \n المعرف: @{username}"""
                 bot.send_message(IsDevloper(), Decor(text, "b"), parse_mode="HTML")
                 for i in IdsAdmins():
                     try:
